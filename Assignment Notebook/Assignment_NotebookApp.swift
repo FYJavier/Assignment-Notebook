94624a977fa10cf81443147ec1de72f55cfc46d1
Freya Javier <thesleepyunicorn63@gmail.com>
2022-07-26 07:19:44 -0500

    Initial Commit

---
+++ Assignment Notebook/Assignment_NotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_NotebookApp.swift
+//  Assignment Notebook
+//
+//  Created by Yuna on 7/26/22.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_NotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
